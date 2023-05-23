07e2221409c41bfc0153e8dc32885aed6a2e6f41
eskiltorset <44784426+eskiltorset@users.noreply.github.com>
2023-05-23 22:29:52 +0200

    all posts rendered

--- script/script.js
+++ script/script.js
@@ -17,101 +17,94 @@ async function fetchPosts(){
     return post;
 }*/
 
-function renderPostHTML(post){
+/*function renderPostHTML(post){
     const container = document.querySelector(".latest-posts");
  
     const postContainer = document.createElement("div");
     postContainer.classList.add("post");
-    postContainer.id = post[0].id;
- 
-    const title = document.createElement("h2");
-    title.innerText = post[0].title.rendered;
-    postContainer.append(title);
- 
-    for (let i = 0; i < post[0]._links["wp:featuredmedia"].length; i++){
-     const imgData = post[0].featured_media[i];
-     const img = document.createElement("img");
-     img.src = post[0].jetpack_featured_media_url;
-     //img.alt = imgData.jetpack_featured_media_url;
-     postContainer.append(img);
+
+    for (let i = 0; i < post.length; i++){
+        var post = post[i];
+
+        postContainer.id = post.id;
+    
+        const title = document.createElement("h2");
+        title.innerText = post.title.rendered;
+        postContainer.append(title);
+    
+        for (let i = 0; i < post._links["wp:featuredmedia"].length; i++){
+        //const imgData = post.featured_media[i];
+        const img = document.createElement("img");
+        img.src = post.jetpack_featured_media_url;
+        //img.alt = imgData.jetpack_featured_media_url;
+        postContainer.append(img);
+        }
+    
+        const content = document.createElement("h3");
+        content.innerText = post.content.rendered;
+        postContainer.append(content);
+    
+        /*const wrapper = document.createElement("a");
+        wrapper.classList.add("details-btn");
+        wrapper.href = `/details.html?id=${id}`;
+        wrapper.innerText = "post Info"; 
+        postContainer.append(wrapper);
     }
- 
-    const content = document.createElement("h3");
-    content.innerText = post[0].content.rendered;
-    postContainer.append(content);
- 
-    /*const wrapper = document.createElement("a");
-    wrapper.classList.add("details-btn");
-    wrapper.href = `/details.html?id=${id}`;
-    wrapper.innerText = "post Info"; 
-    postContainer.append(wrapper);*/
- 
  
     container.append(postContainer);
  
- }
+ }*/
  
  function renderPostsHTML(posts){
      for (let i = 0; i < posts.length; i++){
-         const post = posts[i];
-         renderPostHTML(post);
+        const post = posts[i];
+        //renderPostHTML(post);
      }
+
+    const container = document.querySelector(".latest-posts");
+
+     for (let i = 0; i < posts.length; i++){
+
+        const postContainer = document.createElement("div");
+        postContainer.classList.add("post");
+
+        var post = posts[i];
+
+        postContainer.id = post.id;
+    
+        const title = document.createElement("h2");
+        title.innerText = post.title.rendered;
+        postContainer.append(title);
+    
+        for (let i = 0; i < post._links["wp:featuredmedia"].length; i++){
+        //const imgData = post.featured_media[i];
+        const img = document.createElement("img");
+        img.src = post.jetpack_featured_media_url;
+        //img.alt = imgData.jetpack_featured_media_url;
+        postContainer.append(img);
+        }
+    
+        const content = document.createElement("h3");
+        content.innerText = post.content.rendered;
+        postContainer.append(content);
+    
+        /*const wrapper = document.createElement("a");
+        wrapper.classList.add("details-btn");
+        wrapper.href = `/details.html?id=${id}`;
+        wrapper.innerText = "post Info"; 
+        postContainer.append(wrapper);*/
+
+        container.append(postContainer);
+    }
+    
  }
 
 async function main() {
     const posts = await fetchPosts();
-    //const post = await fetchPost();
-    renderPostHTML(posts);
+    renderPostsHTML(posts);
 }
 
 main();
 
-/*function renderPostHTML(post){
-    //const container = document.querySelector(".latest-posts");
-    const id = post[0].id;
 
-    console.log(id);
- 
-    const postContainer = document.createElement("div");
-    postContainer.classList.add("post");
-    postContainer.id = post.id;
- 
-    const title = document.createElement("h2");
-    title.innerText = post.title;
-    postContainer.append(title);
- 
-    for (let i = 0; i < post.images.length; i++){
-     const imgData = post.images[i];
-     const img = document.createElement("img");
-     img.src = imgData.src;
-     img.alt = imgData.alt;
-     postContainer.append(img);
-    }
- 
-    const content = document.createElement("h3");
-    content.innerText = post.content.rendered;
-    postContainer.append(title);
- 
-    const wrapper = document.createElement("a");
-    wrapper.classList.add("details-btn");
-    wrapper.href = `/details.html?id=${id}`;
-    wrapper.innerText = "post Info"; 
-    postContainer.append(wrapper);
- 
- 
-    container.append(postContainer);
- 
- }
- 
- function renderPostsHTML(posts){
-     for (let i = 0; i < posts.length; i++){
-         const post = posts[i];
-         createPostHTML(post);
-     }
- }
-
-console.log(fullPageURL);
-
-
-renderPostHTML();*/
 
