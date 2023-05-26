ba5f86739c90ebadb1224ed85ac64f9b44c1ddaf
eskiltorset <44784426+eskiltorset@users.noreply.github.com>
2023-05-26 23:37:48 +0200

    added carousel

--- script/script.js
+++ script/script.js
@@ -77,23 +77,29 @@ async function fetchPost(id){
         const title = document.createElement("h2");
         title.innerText = post.title.rendered;
         postContainer.append(title);
-    
+        
+        const imgDiv = document.createElement("div");
+        imgDiv.classList.add("img-div");
         for (let i = 0; i < post._links["wp:featuredmedia"].length; i++){
         //const imgData = post.featured_media[i];
         const img = document.createElement("img");
         img.src = post.jetpack_featured_media_url;
         //img.alt = imgData.jetpack_featured_media_url;
-        postContainer.append(img);
+        
+        imgDiv.append(img);
+        postContainer.append(imgDiv);
+        
         }
     
-        const content = document.createElement("h3");
+        const content = document.createElement("p");
+        content.classList.add("content-p");
         content.innerText = post.content.rendered;
         postContainer.append(content);
-    
+
         const wrapper = document.createElement("a");
         wrapper.classList.add("details-btn");
         wrapper.href = `/blog-specific.html?id=${id}`;
-        wrapper.innerText = "Read more"; 
+        wrapper.innerText = "Read more >>>"; 
         postContainer.append(wrapper);
 
         container.append(postContainer);
